3b5339a1cd957e29f23e2aab6847021c635110fa
mrfrodo24 <spencer.rhodes2@gmail.com>
2017-06-15 22:05:28 -0400

    General setup for init file

--- init.m
+++ init.m
@@ -1,1 +1,11 @@
-% File to be fleshed out with commands to setup project directory with all necessary sub-directories
+% Create default cache directory and gen_params sub-dir
+mkdir('cache')
+mkdir('cache/gen_params')
+
+% Run pre_processing for first time to set parameters
+pre_processing
+
+% Instructions for using suite
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+disp('Command to run suite: cp3g_interactive_masclab')
+fprintf('\n')
