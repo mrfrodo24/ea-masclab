dae554fffbdb339ba8cff83ab609f53c1572eb8c
Spencer Rhodes <spencer.rhodes2@gmail.com>
2018-03-06 15:01:02 -0500

    small refactor / bugfixes for syncing cached paths

--- sync_cachedpath.m
+++ sync_cachedpath.m
@@ -10,16 +10,12 @@ function [ imgsAdded ] = sync_cachedpath( cachedPath, settings )
 %
 
 %% Change path to the full path to the cached_paths_* folder
-cachedPath = ['cache/cached_paths_' num2str(cachedPath) '/'];
+cachedPath = ['cache' filesep 'cached_paths_' num2str(cachedPath) filesep];
 
 %% Get list of files
 imgFilter = @(d) isempty(regexp(d.name,'CROP_CAM')) && isempty(regexp(d.name,'UNCROP_CAM')) && isempty(regexp(d.name,'TRIPLETS')) && isempty(regexp(d.name,'REJECTS')); 
 disp('Searching for PNGs...')
-if strfind(settings.pathToFlakes, '\')
-    files = rdir([settings.pathToFlakes '**\*.png'], imgFilter, settings.pathToFlakes);
-else
-    files = rdir([settings.pathToFlakes '**/*.png'], imgFilter, settings.pathToFlakes);
-end
+files = rdir([settings.pathToFlakes '**' filesep '*.png'], imgFilter, settings.pathToFlakes);
 
 %% Open new file sync.txt
 fid_sync = fopen([cachedPath 'sync.txt'], 'w');
@@ -51,6 +47,12 @@ for i = 1:numCacheFiles
                 newf = newf{1};
             end
             oldf = regexp(oldLine, settings.mascImgRegPattern, 'match'); oldf = oldf{1};
+
+            % Get directories of old and new file
+            oldf_dir = oldLine(1:strfind(oldLine,oldf)-2);
+            newf_dir = files(j).name(1:strfind(files(j).name,newf)-2);
+
+            % Get identifying masc data from filenames
             oldf_s = parse_masc_filename(oldf);
             newf_s = parse_masc_filename(newf);
         end
@@ -60,20 +62,17 @@ for i = 1:numCacheFiles
            || (newf_s.date == oldf_s.date ...
                && newf_s.imageId >= oldf_s.imageId ...
                && newf_s.camId >= newf_s.camId)
-       
             % Write the old line so it can catch up to new files
-            if j <= length(files) && length(settings.pathToFlakes) < length(files(j).folder)
+       
+            if j <= length(files) && ~strcmp(newf_dir, oldf_dir)
                 % Refresh path in line
-                oldPath = oldLine(1:strfind(oldLine,newf)-2);
-                newPath = files(j).folder(length(settings.pathToFlakes)+1:end);
-                oldLine = strrep(oldLine, oldPath, newPath);
-                clear oldPath newPath;
+                oldLine = strrep(oldLine, oldf_dir, newf_dir);
             end
             fprintf(fid_sync, '%s', oldLine);
             % Also advance newf if they were the same, avoids duplicates
             if newf_s.date == oldf_s.date ...
                && newf_s.imageId == oldf_s.imageId ...
-               && newf_s.camId == newf_s.camId
+               && newf_s.camId == oldf_s.camId
                 j = j + 1; 
             end
             oldLine = fgets(fid_old);
@@ -82,12 +81,9 @@ for i = 1:numCacheFiles
            || (newf_s.date == oldf_s.date ...
                && newf_s.imageId <= oldf_s.imageId ...
                && newf_s.camId < oldf_s.camId)
+            % Brand new file, need to catch up to old files
        
-            % append directory to imgf, if nec.
-            if length(settings.pathToFlakes) < length(files(j).folder)
-                newf = [files(j).folder(length(settings.pathToFlakes)+1:end) '/' newf]; %#ok<AGROW>
-            end
-            fprintf(fid_sync, '%s\t1\t0\t0\t0\t0\n', newf);
+            fprintf(fid_sync, '%s\t1\t0\t0\t0\t0\n', [newf_dir filesep newf]);
             imgsAdded = imgsAdded + 1;
             j = j + 1;
         end
@@ -95,7 +91,7 @@ for i = 1:numCacheFiles
         totalWritten = totalWritten + 1;
            
         % End the file if it's getting too big
-        if totalWritten == 2500
+        if totalWritten == 1000
             fclose(fid_sync); pause(1);
             filesWritten = filesWritten + 1;
             movefile([cachedPath 'sync.txt'], [cachedPath 'f' num2str(filesWritten) '.txt']);
@@ -107,28 +103,35 @@ for i = 1:numCacheFiles
 end
 
 while j <= length(files)
-    % append directory to imgf, if nec.
-    if length(settings.pathToFlakes) < length(files(j).folder)
-        newf = [files(j).folder(length(settings.pathToFlakes)+1:end) '/' newf]; %#ok<AGROW>
+    newf = regexp(files(j).name, settings.mascImgRegPattern, 'match');
+    if length(newf) ~= 1
+        disp(['Corrupted file name: ' files(j).name])
+        j = j + 1;
+        continue;
+    else
+        newf = newf{1};
     end
-    fprintf(fid_sync, '%s\t1\t0\t0\t0\t0\n', newf);
+    newf_dir = files(j).name(1:strfind(files(j).name,newf)-2);
+    fprintf(fid_sync, '%s\t1\t0\t0\t0\t0\n', [newf_dir filesep newf]);
     imgsAdded = imgsAdded + 1;
     j = j + 1;
     
     totalWritten = totalWritten + 1;
            
     % End the file if it's getting too big
-    if totalWritten == 1000
-        fclose(fid_sync);
-        movefile([cachedPath 'sync.txt'], [cachedPath 'f' num2str(i) '.txt']);
+    if totalWritten == 500
+        fclose(fid_sync); pause(1);
+        filesWritten = filesWritten + 1;
+        movefile([cachedPath 'sync.txt'], [cachedPath 'f' num2str(filesWritten) '.txt']);
         fid_sync = fopen([cachedPath 'sync.txt'], 'w');
         totalWritten = 0;
     end
 end
 
+fclose('all'); pause(1);
 if totalWritten > 0
-    fclose(fid_sync);
-    movefile([cachedPath 'sync.txt'], [cachedPath 'f' num2str(numCacheFiles) '.txt']);
+    filesWritten = filesWritten + 1;
+    movefile([cachedPath 'sync.txt'], [cachedPath 'f' num2str(filesWritten) '.txt']);
 end
 
 end
