ba46450be801dc066564d19629054b2bc97baf90
mrfrodo24 <spencer.rhodes2@gmail.com>
2018-03-09 00:36:00 -0500

    remove deprecated masc_process_caller

--- utils/masc_process_caller.sh
+++
@@ -1,4 +0,0 @@
-matlab16a -nojvm -r "cd /home/disk/ivanova2/spencerwork/MATLAB/cp3g_masclab_alta; addpath(genpath('./modules')); module_resume_processing;"
-while [$? -ne 0]; do
-  matlab16a -nojvm -r "cd /home/disk/ivanova2/spencerwork/MATLAB/cp3g_masclab_alta; addpath(genpath('./modules')); module_resume_processing;"
-done
