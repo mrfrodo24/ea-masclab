mrfrodo24/ea-masclab

commit 1ba47b0c15891bff30e64211f046adc33f735e64

ability to sync cached paths, bug fix on load_settings

pre_processing.m
@@ -25,12 +25,14 @@ if exist('cache/gen_params/last_parameters.mat','file')
     
     if s == '1'
         % User chose to do nothing, so just refresh the cache and return
-        load_settings; % Load the settings (either from gen_params or SELECTED_CACHED_PATH)
+        [settings, selectedPath] = load_settings(); % Load the settings (either from gen_params or SELECTED_CACHED_PATH)
+        if selectedPath >= 0, CACHED_PATH_SELECTION = selectedPath; end
         cacheSettings; % Cache the SELECTED_CACHED_PATH to gen_params and the selected path
         return;
         
     elseif s == '2'
-        load_settings;
+        [settings, selectedPath] = load_settings();
+        if selectedPath >= 0, CACHED_PATH_SELECTION = selectedPath; end
         settingsToVariables;
         redefine = 1; %#ok<*NASGU>
         
@@ -164,16 +166,14 @@ if ~isempty(filesInPath)
     end
     
     % Open a new cache file
-    numCacheFiles = 1;
-    fwid = fopen(['cache/cached_paths_' num2str(numPaths) '/f' num2str(numCacheFiles) '.txt'], 'w');
+    fwid = fopen(['cache/cached_paths_' num2str(numPaths) '/f1.txt'], 'w');
     for i = 1:length(filesInPath)
         if ~mod(i,500)
             % Start a new cache file every 500 PNGs...
             % Assuming O(100) cropped flakes for each PNG, that would
             % result in O(50,000) records in each txt file.
             fclose(fwid);
-            numCacheFiles = numCacheFiles + 1;
-            fwid = fopen(['cache/cached_paths_' num2str(numPaths) '/f' num2str(numCacheFiles) '.txt'], 'w');
+            fwid = fopen(['cache/cached_paths_' num2str(numPaths) '/f' num2str(i) '.txt'], 'w');
         end
         fprintf(fwid, '%s\t1\t0\t0\t0\t0\n', filesInPath(i).name);
     end
@@ -383,7 +383,7 @@ function define_params
         if ~files
             imgFilter = @(d) isempty(regexp(d.name,'CROP_CAM')) && isempty(regexp(d.name,'UNCROP_CAM')) && isempty(regexp(d.name,'TRIPLETS')) && isempty(regexp(d.name,'REJECTS')); 
             disp('Searching for PNGs...')
-            if strfind(s, '\')
+            if contains(s, '\')
                 files = rdir([s '**\*.png'], imgFilter, s);
             else
                 files = rdir([s '**/*.png'], imgFilter, s);

cp3g_interactive_masclab.m
@@ -12,6 +12,7 @@ disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
 disp('%%%%%%%%%%% WELCOME TO THE CP3G MASC ANALYTICS SUITE %%%%%%%%%%%')
 disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
 fprintf('\n')
+% TODO Ensure that cache and cache/gen_params exist
 
 
 %% PRE-PROCESSING
@@ -22,7 +23,8 @@ clear; % Use this to clear all the pre_processing data since we're just
        % going to load it into a "settings" struct.
 
 % Load the parameters as chosen by user in pre_processing
-load_settings;
+[settings, selectedPath] = load_settings();
+if selectedPath >= 0, CACHED_PATH_SELECTION = selectedPath; end
 
 
 %% SCAN AND CROP
@@ -72,7 +74,8 @@ end
 %   B) Run default processing on flakes
 %   C) Redefine image processing parameter(s)
 %   D) Output statistics
-%   E) Save and exit program
+%   E) Resync images in selected cached path
+%   F) Save and exit program
 
 disp('%%%%%%%%%% CP3G MASCLAB ANALYTICS %%%%%%%%%%');
 fprintf('\n');
@@ -122,7 +125,8 @@ while 1 % Executes until user choose "Save and Quit" from Menu
         pre_processing();
 
         % Load the parameters as chosen by user in pre_processing
-        load_settings;
+        [settings, selectedPath] = load_settings();
+        if selectedPath >= 0, CACHED_PATH_SELECTION = selectedPath; end
         clearvars -except settings CACHED_PATH_SELECTION
         
         fprintf('\n')
@@ -156,6 +160,29 @@ while 1 % Executes until user choose "Save and Quit" from Menu
         
         fprintf('\n')
         user_choice = 'menu';
+        
+    case 'sync_cached_path'
+        disp('%%%%%%%%%%% SYNC UNCROPPED IMAGES %%%%%%%%%%%')
+        fprintf('\n');
+        disp(['About to sync uncropped images in ' settings.pathToFlakes '!'])
+        fprintf('You have 5 seconds to abort... (Ctrl+C)\n');
+        pause(2)
+        fprintf('3...');
+        pause(1)
+        fprintf('2...');
+        pause(1)
+        fprintf('1...');
+        pause(1)
+        
+        fprintf('\n\nSyncing...\n');
+        imgsAdded = sync_cached_path(settings);
+        
+        fprintf('\nDone! %s uncropped images were cached', imgsAdded);
+        if imgsAdded
+            fprintf(' and are ready to be run through Scan & Crop.\n\n');
+        else
+            fprintf('.\n\n');
+        end
         
     case 'save_and_quit'
         

sync_cached_path.m
@@ -1,0 +1,72 @@
+function [ imgsAdded ] = sync_cached_path( path, settings )
+%SYNC_CACHED_PATH Summary of this function goes here
+%
+%   INPUTS:
+%       path - The # of the cached path to sync
+%       settings - Must contain mascImgRegPattern
+%
+%   OUTPUTS:
+%       status - 0 on success, 1 on failure
+%
+
+%% Change path to the full path to the cached_paths_* folder
+path = ['cache/cached_paths_' num2str(path) '/'];
+
+%% Get list of files
+imgFilter = @(d) isempty(regexp(d.name,'CROP_CAM')) && isempty(regexp(d.name,'UNCROP_CAM')) && isempty(regexp(d.name,'TRIPLETS')) && isempty(regexp(d.name,'REJECTS')); 
+disp('Searching for PNGs...')
+if contains(pwd, '\')
+    path = ['cache\cached_paths_' num2str(path) '\'];
+    files = rdir([path '**\*.png'], imgFilter, path);
+else
+    path = ['cache/cached_paths_' num2str(path) '/'];
+    files = rdir([path '**/*.png'], imgFilter, path);
+end
+
+%% Open new file sync.txt
+fid_sync = fopen([path 'sync.txt'], 'w');
+
+%% Loop through current set of cached images
+j = 1; % iterator for `files`
+numWritten = 0;
+imgsAdded = 0;
+for i = 1:length(dir([path 'f*.txt']))
+    copyfile([path 'f' num2str(i) '.txt'], [path 'sync_bak_f' num2str(i) '.txt']);
+    fid_old = fopen([path 'sync_bak_f' num2str(i) '.txt']);
+    oldLine = fgets(fid_old);
+    while oldLine ~= -1
+        % Check if old row matches current img file.
+        % If not, add new file to sync.txt and go to next file
+        % Otherwise, add oldLine to sync.txt and go to next line and next file
+        imgf = regexp(files(j).name, settings.mascImgRegPattern, 'match');
+        if length(imgf) ~= 1
+            disp(['Corrupted file name: ' files(j).name])
+            j = j + 1;
+            continue;
+        else
+            imgf = imgf{1};
+        end
+        
+        % End the file if it's getting too big
+        if numWritten == 500
+            fclose(fid_sync);
+            movefile([path 'sync.txt'], [path 'f' num2str(i) '.txt']);
+            fid_sync = fopen([path 'sync.txt'], 'w');
+            numWritten = 0;
+        end
+        
+        if ~contains(oldLine, imgf)
+            fprintf(fid_sync, '%s\t1\t0\t0\t0\t0\n', imgf);
+            imgsAdded = imgsAdded + 1;
+        else
+            fprintf(fid_sync, '%s', oldLine);
+            oldLine = fgets(fid_old);
+        end
+        numWritten = numWritten + 1;
+        j = j + 1;
+    end
+    fclose(fid_old);
+end
+
+end
+

utils/CP3G_MascLab_Analytics_Menu.m
@@ -13,12 +13,13 @@ fprintf('\t(1) Select module(s) to process\n');
 fprintf('\t(2) Run Scan & Crop\n');
 fprintf('\t(3) Redefine processing parameters\n');
 fprintf('\t(4) Export directory''s statistics\n');
-fprintf('\t(5) Save and exit\n');
+fprintf('\t(5) Resync images in selected cached path\n');
+fprintf('\t(6) Save and exit\n');
 fprintf('\n');
 
 s = input('Enter the number for your selection: ','s');
 % Continue to prompt user until they enter the number of one of the choices
-while isempty(str2num(s)) || isempty(find(str2num(s) == [1 2 3 4 5], 1)) %#ok<ST2NM>
+while isempty(str2num(s)) || isempty(find(str2num(s) == [1 2 3 4 5 6], 1)) %#ok<ST2NM>
     s = input('Enter the number for your selection: ','s');
 end
 
@@ -36,6 +37,9 @@ switch(str2num(s)) %#ok<ST2NM>
         user_input = 'export_stats';
         
     case 5
+        user_input = 'sync_cached_path';
+        
+    case 6
         user_input = 'save_and_quit';
 end
 

utils/load_settings.m
@@ -1,13 +1,19 @@
-% Script to load cp3g-masclab settings
+function [ settings, CACHED_PATH_SELECTION ] = load_settings()
+%LOAD_SETTINGS load cp3g-masclab settings
 %
 % Use optional setting to pre-select a cached path for processing
+settings = struct;
 if exist('CACHED_PATH_SELECTION', 'var')
-    load(['cache/cached_paths_' num2str(CACHED_PATH_SELECTION) '/last_parameters.mat'])
+    load(['cache/cached_paths_' num2str(CACHED_PATH_SELECTION) '/last_parameters.mat']) %#ok<NODEF>
 else
     load('cache/gen_params/last_parameters.mat')
     if exist('CACHED_PATH_SELECTION', 'var')
-        load(['cache/cached_paths_' num2str(CACHED_PATH_SELECTION) '/last_parameters.mat'])
+        load(['cache/cached_paths_' num2str(CACHED_PATH_SELECTION) '/last_parameters.mat']) %#ok<NODEF>
+    else
+        CACHED_PATH_SELECTION = -1;
     end
 end
 
 fprintf('Selected path to flakes: %s\n\n', settings.pathToFlakes);
+
+end

commit 26a25959ef508ee338a7840a7c560ef5e88793cc

fix pre_processing not returning selection as struct... - also undo bug introduced by 1ba47b0

pre_processing.m
@@ -15,7 +15,8 @@ if nargin == 1 && varargin{1} > 0
     load(['cache/cached_paths_' num2str(varargin{1}) '/last_parameters.mat'], ...
         'settings', 'CACHED_PATH_SELECTION')
     fprintf('Selected path to flakes: %s\n\n', settings.pathToFlakes);
-    selection = struct('id', num2str(varargin{1}), 'path', settings.pathToFlakes);
+    selection.id = num2str(varargin{1});
+    selection.path = settings.pathToFlakes;
     return;
 end
     
@@ -45,10 +46,9 @@ if exist(CACHED_PATHS_TXT,'file')
     else
         pathToFlakes = selection.path;
     end
-    selection = selection.id;
 else
     define = 1;
-    selection = 1; % User is running for the first time, so will be the first cached_path
+    selection.id = 1; % User is running for the first time, so will be the first cached_path
 end
 clear s
 
@@ -174,6 +174,7 @@ if ~isempty(filesInPath)
     end
     
     % Open a new cache file
+    numCacheFiles = 1;
     fwid = fopen(['cache/cached_paths_' num2str(numPaths) '/f1.txt'], 'w');
     for i = 1:length(filesInPath)
         if ~mod(i,500)
@@ -181,7 +182,8 @@ if ~isempty(filesInPath)
             % Assuming O(100) cropped flakes for each PNG, that would
             % result in O(50,000) records in each txt file.
             fclose(fwid);
-            fwid = fopen(['cache/cached_paths_' num2str(numPaths) '/f' num2str(i) '.txt'], 'w');
+            numCacheFiles = numCacheFiles + 1;
+            fwid = fopen(['cache/cached_paths_' num2str(numPaths) '/f' num2str(numCacheFiles) '.txt'], 'w');
         end
         fprintf(fwid, '%s\t1\t0\t0\t0\t0\n', filesInPath(i).name);
     end
@@ -199,10 +201,11 @@ disp('Caching these parameters...')
 cacheSettings;
 
     function cacheSettings
-        CACHED_PATH_SELECTION = selection;
+        CACHED_PATH_SELECTION = selection.id;
         clearvars -except settings CACHED_PATH_SELECTION
         save(['cache/cached_paths_' num2str(CACHED_PATH_SELECTION) '/last_parameters.mat'])
-        selection = CACHED_PATH_SELECTION;
+        selection.id = CACHED_PATH_SELECTION;
+        selection.path = settings.pathToFlakes;
     end
 
     function [settings] = paramsAsStruct